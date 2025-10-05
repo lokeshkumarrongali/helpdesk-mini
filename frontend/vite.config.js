5479741d44d2b223a1e27bb7aea8e4e4eb8f1980
lokeshkumarrongali <118551557+lokeshkumarrongali@users.noreply.github.com>
2025-10-05 12:15:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,14 +1,20 @@
+// frontend/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  
+  // For local development only
   server: {
     proxy: {
       '/auth': 'http://localhost:4000',
       '/tickets': 'http://localhost:4000',
       '/comments': 'http://localhost:4000',
-      '/logs': 'http://localhost:4000'
-    }
-  }
+      '/logs': 'http://localhost:4000',
+    },
+  },
+
+  // For deployment
+  base: '/', // VERY important for serving from root
 });
